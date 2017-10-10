52908d37b8fc28cdaf36e348beccf9a40f57efac
David Grant <mrdavidgrant@outlook.com>
2017-10-10 11:33:04 -0400

    Exercise 1 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Burnaby = Store.new
+Burnaby.name = 'Burnaby'
+Burnaby.annual_revenue = 300000
+Burnaby.mens_apparel = true
+Burnaby.womens_apparel = true
+Burnaby.save
+
+Richmond = Store.new
+Richmond.name = 'Richmond'
+Richmond.annual_revenue = 1260000
+Richmond.mens_apparel = false
+Richmond.womens_apparel = true
+Richmond.save
+
+Gastown = Store.new
+Gastown.name = 'Gastown'
+Gastown.annual_revenue = 190000
+Gastown.mens_apparel = true
+Gastown.womens_apparel = false
+Gastown.save
+
+puts "#{Store.count}"
